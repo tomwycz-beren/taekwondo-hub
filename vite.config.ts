8396df7caa556bd6caf75db5776b4fe241d976c0
tom <tomasz.wyczolkowski@tessel.pl>
2026-02-19 22:36:51 +0100

    build changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+base: '/taekwondo-hub/',  // nazwa repo!
   server: {
     host: "::",
     port: 8080,
